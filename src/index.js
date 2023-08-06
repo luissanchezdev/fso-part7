811d8f7b0f9d488d9b6edef18e4f1d298153cbaa
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-08-05 23:48:05 -0500

    change: show login link

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ import {
   Navigate,
   useParams,
   useNavigate,
+  useMatch
 } from "react-router-dom"
 
 
 const Home = () => (
   <div>
-    <h2>notes app</h2>
+    <h2>TKTL notes app</h2>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </div>
 )
 
-const Note = ({ notes }) => {
-  const id = useParams().id
-  const note = notes.find(n => n.id === Number(id))
+const Note = ({ note }) => {
+
   return (
     <div>
       <h2>{note.content}</h2>
@@ -60,7 +60,7 @@ const Login = (props) => {
 
   const onSubmit = (event) => {
     event.preventDefault()
-    props.onLogin('mluukkai')
+    props.onLogin('luis')
     navigate('/')
   }
 
@@ -104,6 +104,13 @@ const App = () => {
 
   const [user, setUser] = useState(null)
 
+  const match = useMatch('/notes/:id')
+
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
+
   const login = (user) => {
     setUser(user)
   }
@@ -114,27 +121,24 @@ const App = () => {
 
   return (
     <div>
-      <Router>
-        <div>
-          <Link style={padding} to="/">home</Link>
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
-        </div>
-
-        <Routes>
-          <Route path="/notes/:id" element={<Note notes={notes} />} />
-          <Route path="/notes" element={<Notes notes={notes} />} />
-          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
-          <Route path="/login" element={<Login onLogin={login} />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </div>
+      <Routes>
+        <Route path="/notes/:id" element={<Note note={note} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login onLogin={login} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
